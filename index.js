1068016f88c177559c0b85ada71a2559dce7d294
bleeccs <bleeccs@gmail.com>
2021-10-31 00:41:48 -0700

    Setting up authentication

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
+require("dotenv").config()
+
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
 
-app.get('/',  (request, response) => {
-    response.send('Hello world');
-});
+const port = process.env.PORT || 3000
 
-app.get ('/status', (request, response) => {
-    response.status(200).json({message:'ok', status:200});
-});
+const routes = require("./routes/main")
+const passwordRoutes = require("./routes/password")
 
-app.post('/signup',  (request, response) => {
-    response.status(200).json({message: 'ok', status: 200});
-});
+// update express settings
+app.use(express.urlencoded({ extended: false })); // parse application/x-www-form-urlendcoded
+app.use(express.json()); // parse application/json
+app.use(cookieParser())
+app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN}))
+
+// require passport auth
+require("./auth/auth")
+
+// setup routes
+app.use("/", routes)
+app.use("/", passwordRoutes)
+
+// Catch all other routes
+app.use((request, response) => {
+    response.status(404).json({message: '404 - Not Found', status: 404})
+})
+
+// Catch errors
+app.use((error, request, response, next) => {
+    console.log(error)
+    response.status(error.status || 500).json({error: error.message, status: 500})
+})
 
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`)
